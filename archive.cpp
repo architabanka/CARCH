be59ee0911e3bc65f88852037f682a8ca9ce3f3a
A-Prelude-To-Chaos <cs25b042@smail.iitm.ac.in>
2026-04-01 22:36:27 +0530

    Check for carch-2nd time

--- archive.cpp
+++ archive.cpp
@@ -104,10 +104,13 @@ namespace Archive {
         }
 
         char magic[5];
-        //ADD : check for CARCH, giver error if wrong
         in.read(magic, 5);
-        if(!(std::memcmp(magic,"CARCH",5)== 0)){
-            std::cerr << "NOT A CARCH FILE!~" << std::endl;
+        if (!in) {
+            std::cerr << "[!]TOo small" << std::endl;
+            return;
+        }
+        if (std::memcmp(magic, "CARCH", 5) != 0) {
+            std::cerr << "[!] NOT A CARCH FILE!~" << std::endl;
             return;
         }
         uint32_t file_count;
