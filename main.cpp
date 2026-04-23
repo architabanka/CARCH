877f4a137a5b7f3bd088599410e74c49fef465a8
Bhupathi-Nithin-Agnihotri <bhupathinithin2008@gmail.com>
2026-04-23 13:30:04 +0530

    Fixed Error due to eof flag and row major ordering in state

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int main(int argc, char** argv) {
         cerr << "Usage:" << endl;
         cerr << "  " << argv[0] << " c <archive.carch> <file1> <file2> ..." << endl;
         cerr << "  " << argv[0] << " x <archive.carch>" << endl;
+        cerr << "  " << argv[0] << " e <fileName>" <<endl;
+        cerr << "  " << argv[0] << " d <fileName>" <<endl;
         return 1;
     }
 
@@ -26,31 +28,54 @@ int main(int argc, char** argv) {
             }
             Archive::createArchive(archiveName, fileNames);
             string password;
+            cout << "Enter the Password: ";
             cin >> password;
             if (encrypt(archiveName, password) != 0) {
                 cerr << "Error in encryption";
                 return -1;
             }
         } else if (cmd == "x") {
-            string tempName = "_carch_" + archiveName + ".txt";
+            string tempName = "Decrypted_Decompressed_carch_" + archiveName + "_aes128_huffman_ushfkjsjdhfdhk";
             string password;
+            cout << "Enter the Password: ";
             cin >> password;
             if (decrypt(archiveName, tempName, password) != 0) {
                 cerr << "Error in decryption";
                 return -1;
             }
             Archive::extractArchive(tempName);
-            // if (!filesystem::remove(tempName)) {
-            //    cout<<"Dont delete the file"<< tempName << "while the process is going on.";
-            // } 
+            if (!filesystem::remove(tempName)) {
+                cout<<"Dont delete the file"<< tempName << "while the process is going on.";
+            }
+        } else if (cmd == "e") {
+            string password;
+            cout << "Enter the Password: ";
+            cin >> password;
+            if (encrypt(archiveName, password) != 0) {
+                cerr << "Error in encryption";
+                return -1;
+            }
+        } else if (cmd == "d") {
+            string password;
+            cout << "Enter the Password: ";
+            cin >> password;
+            if (decrypt(archiveName, "carch_decrypted_" + archiveName  + "_sufhewuifhcxjknco", password) != 0) {
+                cerr << "Error in decryption";
+                return -1;
+            }
+        } else {
+            cerr << "Error: Unknown Command" << endl;
+            cerr << "Usage:" << endl;
+            cerr << "  " << argv[0] << " c <archive.carch> <file1> <file2> ..." << endl;
+            cerr << "  " << argv[0] << " x <archive.carch>" << endl;
+            cerr << "  " << argv[0] << " e <fileName>" <<endl;
+            cerr << "  " << argv[0] << " d <fileName>" <<endl;
+            return -1;
         }
-        else {
-            cerr << "Unknown command: " << cmd << endl;
-            return 1;
-        }
-    } catch (const exception& e) {
+    } 
+    catch (const exception& e) {
         cerr << "Exception: " << e.what() << endl;
-        return 1;
+        return -1;
     }
 
     return 0;
